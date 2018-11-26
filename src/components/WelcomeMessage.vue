98fdea0254257592abfad47c357bc4c2cf285d5c
Kevez <kbrady08@gmail.com>
2018-11-26 14:43:57 +0000

    Added component which displays a welcome message to the user once logged in

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="welcome-message">
+        Welcome back, {{username}} [<a href="#" @click="logout()">Logout</a>]
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'WelcomeMessage',
+        props: ['username'],
+		methods: {
+			login() {
+				if (this.username.length === 0) {
+					return;
+				}
+
+				this.$store.dispatch('login', {
+					username: this.username
+				});
+			},
+			logout() {
+				this.$store.dispatch('logout');
+			},
+			checkKeyPress: function (e) {
+				if (e.keyCode !== 13) {
+					return;
+				}
+
+				this.login();
+			}
+		}
+	}
+</script>
+
+<style scoped>
+    .welcome-message {
+        margin-top: 0;
+        text-align: right;
+        padding: 1rem;
+        background: #0079bf;
+        color: #fff;
+    }
+</style>
