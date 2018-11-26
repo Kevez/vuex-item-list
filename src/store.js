897e1b2bbb3ca781dbffb10a8080a71e6b6d4b2b
Kevez <kbrady08@gmail.com>
2018-11-26 14:36:08 +0000

    Populate state with user and item objects

--- src/store.js
+++ src/store.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+var id;
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+	state: {
+		user: {
+			isLoggedIn: false,
+			name: '',
+			level: 1
+		},
+		items: [
+			{id: 1, parent: 0, name: 'Default item 1'},
+			{id: 2, parent: 0, name: 'Default item 2'},
+			{id: 3, parent: 0, name: 'Default item 3'},
+			{id: 4, parent: 0, name: 'Default item 4'},
+			{id: 5, parent: 0, name: 'Default item 5'}
+		],
+	},
+	mutations: {},
+	actions: {}
 })
