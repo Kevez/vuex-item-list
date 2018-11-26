ef9b2bd41ea25dbb8b12c008e675b8c11cf2fb6f
Kevez <kbrady08@gmail.com>
2018-11-26 16:44:17 +0000

    Configure sockets

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
+import VueSocketIO from 'vue-socket.io';
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueSocketIO, 'http://localhost:7777');
 
 new Vue({
-  store,
-  render: h => h(App)
+	store,
+	render: h => h(App)
 }).$mount('#app')
