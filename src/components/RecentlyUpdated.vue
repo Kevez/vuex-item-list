276725f576914feaa1f9c9288dbd0beb875392b3
Kevez <kbrady08@gmail.com>
2018-11-26 14:59:20 +0000

    Added component to show recently updated items. Removed logo from seed project.

---
+++ src/components/RecentlyUpdated.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="recently-updated">
+        {{message}}
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'RecentlyUpdated',
+        props: ['message'],
+	}
+</script>
+
+<style scoped>
+    .recently-updated {
+        background: #c5f5c5;
+        margin-bottom: 1rem;
+        padding: 1rem;
+    }
+</style>
