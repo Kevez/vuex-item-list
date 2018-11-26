e3ba9c1e96faae19f004618d3a4c358982f6df7f
Kevez <kbrady08@gmail.com>
2018-11-26 16:47:15 +0000

    Added basic node server with socket.io

---
+++ server.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 7777;
+
+io.on('connection', function (socket) {
+	socket.on('addItem', (payload) => {
+		socket.broadcast.emit('addItem', payload);
+		io.emit('itemUpdated', {
+			type: 'add',
+			payload: payload
+		});
+	});
+
+	socket.on('editItem', (payload) => {
+		socket.broadcast.emit('editItem', payload);
+		io.emit('itemUpdated', {
+			type: 'edit',
+			payload: payload
+		});
+	});
+
+	socket.on('removeItem', (payload) => {
+		socket.broadcast.emit('removeItem', payload);
+		io.emit('itemUpdated', {
+			type: 'remove',
+			payload: payload
+		});
+	});
+});
+
+http.listen(port, function () {
+	console.log(`Server listening on localhost:${port}`);
+});
